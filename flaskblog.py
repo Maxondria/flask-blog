15bf2a6abab61ad9c3b39767cf848ad3ab1a1407
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-02-04 22:38:26 +0300

    +feat(): add a nav bar, link main.css to layout, add dummy content to home.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+posts = [
+    {
+        "author": "Lars Vondracek",
+        "title": "Trance",
+        "content": "nulla nisl nunc nisl duis bibendum felis sed t",
+        "date_posted": "2019-09-08"
+    },
+    {
+        "author": "Portia Stennard",
+        "title": "Quiet Flows the Don (Tikhiy Don)",
+        "content": "luctus ultricies eu nibh quisque id justo sit amet sapien ",
+        "date_posted": "2020-01-08"
+    },
+]
 
 app = Flask(__name__)
 
@@ -6,12 +21,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
