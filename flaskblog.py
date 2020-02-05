d4e2f3e1e977c25a0303f22e1a18215be879a2a1
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-02-05 13:15:20 +0300

    +feat(): add login and register forms with validation, display flash messages, mimick register and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 posts = [
     {
@@ -17,6 +18,8 @@ posts = [
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'ee9ce57217cad182c77c88b36f62e79c'
+
 
 @app.route('/')
 @app.route('/home')
@@ -27,6 +30,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'pass':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Authentication Failed', 'danger')
+    return render_template('login.html', title='LogIn', form=form)
 
 
 if __name__ == "__main__":
