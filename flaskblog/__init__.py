4a71303a40323a15bcd03a4ba2bbab310396ad8d
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-02-10 15:25:13 +0300

    +feat(): config email sending environment

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,13 +1,22 @@
+import os
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'ee9ce57217cad182c77c88b36f62e79c'
+app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -15,6 +24,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login' # Function name of a route
 login_manager.login_message = 'Please login first!'
 login_manager.login_message_category = 'info'
+mail = Mail(app)
 
 
 @app.before_first_request
